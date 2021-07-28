077c2931edc69cf8830c303e856714e3eb50250f
VijayThummaCTS <88023383+VijayThummaCTS@users.noreply.github.com>
2021-07-28 21:01:10 +0530

    Demorest
    Demorest

---
+++ restapidemo/src/main/java/com/springboot/restapidemo/EmployeeDAO.java
@@ -0,0 +1,53 @@
+package com.springboot.restapidemo;
+
+import org.springframework.stereotype.Repository;
+
+// Importing the employees class to use the defined properties in this class
+
+import com.springboot.restapidemo.Employees;
+
+@Repository
+
+// Class to create a list of employees
+public class EmployeeDAO {
+
+    private static Employees list = new Employees();
+
+    // This static block is executed before executing the main block
+    static
+    {
+        // Creating a few employees and adding them to the list
+        list.getEmployeeList().add(
+                new Employee(
+                        1,
+                        "Tony",
+                        "Stark",
+                        "ironman@avengers.com"));
+
+        list.getEmployeeList().add(
+                new Employee(
+                        2, "Bruce",
+                        "Banner",
+                        "hulk@avengers.com"));
+
+        list.getEmployeeList().add(
+                new Employee(
+                        3, "Steve",
+                        "Rogers",
+                        "capamerica@avengers.com"));
+    }
+
+    // Method to return the list
+    public Employees getAllEmployees()
+    {
+        return list;
+    }
+
+    // Method to add an employee
+    // to the employees list
+    public void addEmployee(Employee employee)
+    {
+        list.getEmployeeList().add(employee);
+
+    }
+}
